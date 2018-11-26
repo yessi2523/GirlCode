03ebee555fca942aebf19defbd27d738246b254f
yessenia Tejeda <tejeda@brandeis.edu>
2018-11-26 11:31:26 -0500

    Latest version

--- foodfeels.py
+++ foodfeels.py
@@ -2,12 +2,15 @@ def number_of_stars(s):
     return
 
 shermanReviews=[]
+shermanComments=[]
 def getShermanReview():
     print("What is your rating of the food in general from 1-5 stars in Sherman?")
     s=input("Enter number of stars from 1-5: ")
-    print("Your rating is " + str(s) + " stars")
+    critique=input("Critique or comments on the food: ")
+    print("Your rating is " + str(s) + " stars and " + str(critique))
 
     shermanReviews.append(int(s))
+    shermanComments.append(critique)
 
 def printShermanReviews():
     print("reviews are :"+str(shermanReviews))
@@ -15,10 +18,13 @@ def printShermanReviews():
     n = len(shermanReviews)
     avg = total/n
     print("The average is " + str(avg) )
+    print(shermanComments)
+
 
 def getReviews():
     getShermanReview()
     printShermanReviews()
+
 
 while (True):
     getReviews()
--- usdanreviews.py
+++ usdanreviews.py
@@ -2,12 +2,15 @@ def number_of_stars(s):
     return
 
 usdanReviews=[]
+usdanComments=[]
 def getusdanReview():
     print("What is your rating of the food in general from 1-5 stars in Sherman?")
     s=input("Enter number of stars from 1-5: ")
-    print("Your rating is " + str(s) + " stars")
+    critique=input("Critique or comments on the food: ")
+    print("Your rating is " + str(s) + " stars" + str(critique))
 
     usdanReviews.append(int(s))
+    usdanComments.append(critique  )
 
 def printusdanReviews():
     print("reviews are :"+str(usdanReviews))
@@ -15,6 +18,7 @@ def printusdanReviews():
     n = len(usdanReviews)
     avg = total/n
     print("The average is " + str(avg) )
+    print(usdanComments)
 
 def getReviews2():
     getusdanReview()
