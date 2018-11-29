69a6ac6e8861e06f708797602be3ab439b2c843a
yessenia Tejeda <tejeda@brandeis.edu>
2018-11-29 11:35:32 -0500

    new changes

--- foodfeels.py
+++ foodfeels.py
@@ -1,8 +1,34 @@
+# from flask import Flask
+# app = Flask(__name__)
+#
+# @app.route("/")
+# def index():
+#     return "Index!"
+#
+# @app.route("/hello")
+# def hello():
+#     return "Hello World!"
+#
+# @app.route("/members")
+# def members():
+#     return "Members"
+#
+# @app.route("/members/<string:name>/")
+# def getMember(name):
+#     return name
+#
+# if __name__ == "__main__":
+#     app.run()
+
+import datetime
+
 def number_of_stars(s):
     return
 
 shermanReviews=[]
 shermanComments=[]
+meal="dinner"
+
 def getShermanReview():
     print("What is your rating of the food in general from 1-5 stars in Sherman?")
     s=input("Enter number of stars from 1-5: ")
@@ -20,8 +46,26 @@ def printShermanReviews():
     print("The average is " + str(avg) )
     print(shermanComments)
 
+def checktime():
+    global meal
+    global shermanReviews
+    global shermanComments
+    t=datetime.datetime.now()
+    if t.hour>16 and meal=="lunch":
+        meal="dinner"
+        shermanReviews=[]
+        shermanComments=[]
+        print("It's dinner time! ")
+    elif t.hour<=16 and meal=="dinner":
+        meal="lunch"
+        shermanReviews=[]
+        shermanComments=[]
+        print("It's lunch time! ")
+
 
 def getReviews():
+    checktime()
+    print(meal)
     getShermanReview()
     printShermanReviews()
 
--- usdanreviews.py
+++ usdanreviews.py
@@ -1,28 +1,56 @@
+import datetime
 def number_of_stars(s):
     return
 
 usdanReviews=[]
 usdanComments=[]
+meal="dinner"
+
 def getusdanReview():
-    print("What is your rating of the food in general from 1-5 stars in Sherman?")
+    print("What is your rating of the food in general from 1-5 stars in Usdan?")
     s=input("Enter number of stars from 1-5: ")
     critique=input("Critique or comments on the food: ")
-    print("Your rating is " + str(s) + " stars" + str(critique))
+    print("Your rating is " + str(s) + " stars: " + str(critique))
 
     usdanReviews.append(int(s))
-    usdanComments.append(critique  )
+    usdanComments.append(critique)
 
 def printusdanReviews():
     print("reviews are :"+str(usdanReviews))
     total = sum(usdanReviews)
     n = len(usdanReviews)
+    if n==0:
+        print("There are no reviews yet ")
+        return
     avg = total/n
     print("The average is " + str(avg) )
     print(usdanComments)
 
+
+def checktime():
+    global meal
+    global usdanReviews
+    global usdanComments
+    t=datetime.datetime.now()
+    if t.hour>16 and meal=="lunch":
+        meal="dinner"
+        usdanReviews=[]
+        usdanComments=[]
+        print("It's dinner time! ")
+    elif t.hour<=16 and meal=="dinner":
+        meal="lunch"
+        usdanReviews=[]
+        usdanComments=[]
+        print("It's lunch time! ")
+
+
+
 def getReviews2():
-    getusdanReview()
+    checktime()
+    print(meal)
     printusdanReviews()
+    getusdanReview()
+
 
 while (True):
     getReviews2()
